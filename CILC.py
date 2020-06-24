e2f1e776af2bf08fc9293cc8d7ba055a8713a3dc
Maude Charmetant <58000492+MaudeCharmetant@users.noreply.github.com>
2020-06-24 16:47:20 +0200

    Update CILC.py

--- CILC.py
+++ CILC.py
@@ -72,3 +72,232 @@ def smooth2reso(wt_reso,ori_reso,Hmap):
     return stacked   
     
     
+def map2fields(maps_array,It,nfields,wt_reso,dic_reso,median,gauss,mask,dic_freq): 
+    
+    """
+    Function which read maps at different frequencies, smooth them to a desired resolution and merge them 
+    into a big matrix composed of each pixel for each healpy field and for each frequency. 
+
+    Parameters
+    ----------
+    
+    maps_array : array 
+        Containing all the healpy maps we want to perform the ILC on.
+    It : int 
+        Number of pixels per field.
+    nfields : int 
+        Number of fields. 
+    wt_reso : float
+        Desired resolutions of the maps in arcmin. 
+    dic_reso : dictonary 
+        Dictonary containing all 
+    median : bool 
+        If True remove the median of each field. Otherwise fit a Gaussian to the pixel histogram 
+        and remove the bias of each field.
+         
+    Returns
+    -------
+    ndarray
+        Containing in the row the number of pixels in a field, each column is a field and the tird dimesion
+        is the number of frequencies.  
+
+    """
+        
+    cube = np.empty([int(It),int(nfields),int(len(dic_freq))])
+
+    for i in range(len(maps_array)):
+                 
+        Fmap = smooth2reso(wt_reso=wt_reso,ori_reso=dic_reso[i],Hmap=maps_array[i])
+        
+        if mask is not None: 
+            
+            Fmap *= mask 
+        
+        else: 
+            
+            Fmap = Fmap 
+        
+        k=0
+        for j in range(0,len(Fmap),It):  
+                
+                #if median == True: 
+                    
+                median = np.median(Fmap[j:j+It-1])
+                cube[0:It-1,k,i]+= Fmap[j:j+It-1] - [median]*(It-1)
+                k = k+1   
+                    
+                #else: 
+                    
+                #offset = sz.create_histogram(Fmap[j:j+It-1], int(np.sqrt(np.size(Fmap[j:j+It-1]))), fit=True, plot=False) 
+                #cube[0:It-1,k,i]+= Fmap[j:j+It-1] - [offset[0]]*(It-1)
+                #k = k+1  
+
+    return cube 
+
+def covcorr_matrix(data,rowvar,mask,dic_freq):
+    
+    """
+    Function which compute the covariance matrix and the correlation matrix.  
+
+    Parameters
+    ----------
+    
+    data : array
+        Array containing the data we want to take the covariance matrix of. 
+    rowvar : bool
+        If rowvar is True (default), then each row represents a variable, with observations in the columns. 
+        Otherwise, the relationship is transposed: each column represents a variable, while the rows contain
+        observations.
+    mask : bool
+        If True compute the covariance matrix only on the non-masked pixels. Otherwise on all the array.
+    dic_freq : dictonary 
+        Dictonary containing all the frequencies we want to compute the covariance matrix on.
+        
+    Returns
+    -------
+    tuple
+        containing [0] : the covariance matrix [1] : the correlation matrix
+
+    """
+    
+    #Compute the covariance matrix : 
+    if mask is not None: 
+        for i in range(len(dic_freq)):
+            not_masked = np.where(data[:,i] != 0)[0]
+        data = data[not_masked]
+        
+    cov_matrix = np.cov(data,y = None, rowvar = rowvar) #Compute the covariance matrix 
+
+    #Compute the correlation matrix : 
+    corr_matrix = np.corrcoef(data, y=None, rowvar=rowvar) #Compute the correlation matrix 
+    
+    return (cov_matrix,corr_matrix)
+
+def D_I_CMB(x):
+    
+    """
+    Function which compute the CMB spectral shape. 
+
+    Parameters
+    ----------
+    
+    x : array
+        Frequency range over which the CMB spectral shape will be computed. 
+        
+    Returns
+    -------
+    array
+        Array contaning the variarion of intensity produced by CMB over the fequencies. 
+
+    """
+    
+    #Compute Delta I :  
+    x_nu = np.array((cst.h.value*x)/(cst.k_B.value*T_CMB)) 
+    A=np.array((2*cst.h.value*x**3)/(cst.c.value**2))      
+    Delta_I = A/(np.exp(x_nu)-1)
+    
+    #Give feedback to the operator : 
+    print("Delta I as been computed ")
+    
+    return   Delta_I
+
+def D_I_tSZ(x,y):
+    
+    """
+    Function which compute the tSZ spectral shape. 
+
+    Parameters
+    ----------
+    
+    x : array
+        Frequency range over which the tSZ spectral shape will be computed. 
+    y : float
+        Value of the Compton-y parameter assumed here. 
+        
+    Returns
+    -------
+    array
+        Array contaning the Variarion of intensity produced by tSZ over the fequencies. 
+
+    """
+    
+    #Compute Delta I : 
+    I_0 = (2*(cst.k_B.value*T_CMB)**3)/(cst.h.value*cst.c.value)**2    
+    x_nu = np.array((cst.h.value*x)/(cst.k_B.value*T_CMB))    
+    Delta_I = np.array(I_0*y*(x_nu**4)*(np.exp(x_nu)/(np.exp(x_nu)-1)**2)*((x_nu*(np.exp(x_nu)+1)/(np.exp(x_nu)-1))-4))
+    
+    #Give feedback to the operator : 
+    print("Delta I as been computed ")
+    
+    return   Delta_I
+
+def mixing_vector_CMB(dic_freq):
+    
+    """
+    Function which compute the multiplying vector to transform a y-map into a tSZ(f). 
+
+    Parameters
+    ----------
+    
+    dic_freq : dic
+        Dictonary containing the frequencies we want to get a tSZ map of.  
+        
+    Returns
+    -------
+    array
+        Array contaning the multiplying vector. 
+
+    """       
+    
+    #Initilisation : 
+    freq = np.arange(0,1000)*1e9
+    mix_vect = []
+    
+    # Compute the spectral shape of tSZ : 
+    Delta_I = D_I_CMB(freq)
+
+    #For each frequency channel, compute Delta_I : 
+    for i in range(len(dic_freq)):
+        
+        mix_vect.append(Delta_I[dic_freq[i]]*(1e20))
+        
+    #Give feeback to the operator :    
+    print('The mixing vector is : ',mix_vect)
+
+    return mix_vect
+
+def mixing_vector_tSZ(dic_freq):
+    
+    """
+    Function which compute the multiplying vector to transform a y-map into a tSZ(f). 
+
+    Parameters
+    ----------
+    
+    dic_freq : dic
+        Dictonary containing the frequencies we want to get a tSZ map of.  
+        
+    Returns
+    -------
+    array
+        Array contaning the multiplying vector. 
+
+    """       
+    
+    #Initilisation : 
+    freq = np.arange(0,1000)*10**9  
+    mix_vect = []
+    
+    # Compute the spectral shape of tSZ : 
+    Delta_I = D_I_tSZ(freq,1)
+
+    #For each frequency channel, compute Delta_I : 
+    for i in range(len(dic_freq)):
+        
+        mix_vect.append(Delta_I[dic_freq[i]]*(1e20))
+        
+    #Give feeback to the operator :    
+    print('The mixing vector is : ',mix_vect)
+
+    return mix_vect
+
